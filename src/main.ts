27586037ab1bb9360e3ed4ae4333346ea37f0b28
板橋 清信 <itahashi_kiyonobu@cyberagent.co.jp>
2018-09-10 15:18:48 +0900

    fix lint

--- src/main.ts
+++ src/main.ts
@@ -1,139 +1,152 @@
-import * as  bodyParser from 'body-parser';
-import * as express from 'express';
-import * as _ from 'lodash';
+import bodyParser from 'body-parser';
+import express, { NextFunction, Request, Response } from 'express';
+import _ from 'lodash';
 import {
-    Block, generateNextBlock, generatenextBlockWithTransaction, generateRawNextBlock, getAccountBalance,
-    getBlockchain, getMyUnspentTransactionOutputs, getUnspentTxOuts, sendTransaction
+  Block,
+  generateNextBlock,
+  generatenextBlockWithTransaction,
+  generateRawNextBlock,
+  getAccountBalance,
+  getBlockchain,
+  getMyUnspentTransactionOutputs,
+  getUnspentTxOuts,
+  sendTransaction,
 } from './blockchain';
-import {connectToPeers, getSockets, initP2PServer} from './p2p';
-import {UnspentTxOut} from './transaction';
-import {getTransactionPool} from './transactionPool';
-import {getPublicFromWallet, initWallet} from './wallet';
+import { connectToPeers, getSockets, initP2PServer } from './p2p';
+import { UnspentTxOut } from './transaction';
+import { getTransactionPool } from './transactionPool';
+import { getPublicFromWallet, initWallet } from './wallet';
 
-const httpPort: number = parseInt(process.env.HTTP_PORT) || 3001;
-const p2pPort: number = parseInt(process.env.P2P_PORT) || 6001;
+const httpPort: number = parseInt(process.env.HTTP_PORT, 10) || 3001;
+const p2pPort: number = parseInt(process.env.P2P_PORT, 10) || 6001;
 
 const initHttpServer = (myHttpPort: number) => {
-    const app = express();
-    app.use(bodyParser.json());
+  const app = express();
+  app.use(bodyParser.json());
 
-    app.use((err, req, res, next) => {
-        if (err) {
-            res.status(400).send(err.message);
-        }
-    });
+  app.use((err, _: Request, res: Response, __: NextFunction) => {
+    if (err) {
+      res.status(400).send(err.message);
+    }
+  });
 
-    app.get('/blocks', (req, res) => {
-        res.send(getBlockchain());
-    });
+  app.get('/blocks', (_: Request, res: Response) => {
+    res.send(getBlockchain());
+  });
 
-    app.get('/block/:hash', (req, res) => {
-        const block = _.find(getBlockchain(), {'hash' : req.params.hash});
-        res.send(block);
-    });
+  app.get('/block/:hash', (req: Request, res: Response) => {
+    const block = _.find(getBlockchain(), { hash: req.params.hash });
+    res.send(block);
+  });
 
-    app.get('/transaction/:id', (req, res) => {
-        const tx = _(getBlockchain())
-            .map((blocks) => blocks.data)
-            .flatten()
-            .find({'id': req.params.id});
-        res.send(tx);
-    });
+  app.get('/transaction/:id', (req: Request, res: Response) => {
+    const tx = _(getBlockchain())
+      .map((blocks: Block) => blocks.data)
+      .flatten()
+      .find({ id: req.params.id });
+    res.send(tx);
+  });
 
-    app.get('/address/:address', (req, res) => {
-        const unspentTxOuts: UnspentTxOut[] =
-            _.filter(getUnspentTxOuts(), (uTxO) => uTxO.address === req.params.address);
-        res.send({'unspentTxOuts': unspentTxOuts});
-    });
+  app.get('/address/:address', (req: Request, res: Response) => {
+    const unspentTxOuts: UnspentTxOut[] = _.filter(
+      getUnspentTxOuts(),
+      (uTxO: UnspentTxOut) => uTxO.address === req.params.address,
+    );
+    res.send({ unspentTxOuts: unspentTxOuts });
+  });
 
-    app.get('/unspentTransactionOutputs', (req, res) => {
-        res.send(getUnspentTxOuts());
-    });
+  app.get('/unspentTransactionOutputs', (_: Request, res: Response) => {
+    res.send(getUnspentTxOuts());
+  });
 
-    app.get('/myUnspentTransactionOutputs', (req, res) => {
-        res.send(getMyUnspentTransactionOutputs());
-    });
+  app.get('/myUnspentTransactionOutputs', (_: Request, res: Response) => {
+    res.send(getMyUnspentTransactionOutputs());
+  });
 
-    app.post('/mineRawBlock', (req, res) => {
-        if (req.body.data == null) {
-            res.send('data parameter is missing');
-            return;
-        }
-        const newBlock: Block = generateRawNextBlock(req.body.data);
-        if (newBlock === null) {
-            res.status(400).send('could not generate block');
-        } else {
-            res.send(newBlock);
-        }
-    });
+  app.post('/mineRawBlock', (req: Request, res: Response) => {
+    if (req.body.data === null) {
+      res.send('data parameter is missing');
+      return;
+    }
+    const newBlock: Block = generateRawNextBlock(req.body.data);
+    if (newBlock === null) {
+      res.status(400).send('could not generate block');
+    } else {
+      res.send(newBlock);
+    }
+  });
 
-    app.post('/mineBlock', (req, res) => {
-        const newBlock: Block = generateNextBlock();
-        if (newBlock === null) {
-            res.status(400).send('could not generate block');
-        } else {
-            res.send(newBlock);
-        }
-    });
+  app.post('/mineBlock', (_: Request, res: Response) => {
+    const newBlock: Block = generateNextBlock();
+    if (newBlock === null) {
+      res.status(400).send('could not generate block');
+    } else {
+      res.send(newBlock);
+    }
+  });
 
-    app.get('/balance', (req, res) => {
-        const balance: number = getAccountBalance();
-        res.send({'balance': balance});
-    });
+  app.get('/balance', (_: Request, res: Response) => {
+    const balance: number = getAccountBalance();
+    res.send({ balance: balance });
+  });
 
-    app.get('/address', (req, res) => {
-        const address: string = getPublicFromWallet();
-        res.send({'address': address});
-    });
+  app.get('/address', (_: Request, res: Response) => {
+    const address: string = getPublicFromWallet();
+    res.send({ address: address });
+  });
 
-    app.post('/mineTransaction', (req, res) => {
-        const address = req.body.address;
-        const amount = req.body.amount;
-        try {
-            const resp = generatenextBlockWithTransaction(address, amount);
-            res.send(resp);
-        } catch (e) {
-            console.log(e.message);
-            res.status(400).send(e.message);
-        }
-    });
+  app.post('/mineTransaction', (req: Request, res: Response) => {
+    const address = req.body.address;
+    const amount = req.body.amount;
+    try {
+      const resp = generatenextBlockWithTransaction(address, amount);
+      res.send(resp);
+    } catch (e) {
+      console.log(e.message);
+      res.status(400).send(e.message);
+    }
+  });
 
-    app.post('/sendTransaction', (req, res) => {
-        try {
-            const address = req.body.address;
-            const amount = req.body.amount;
+  app.post('/sendTransaction', (req: Request, res: Response) => {
+    try {
+      const address = req.body.address;
+      const amount = req.body.amount;
 
-            if (address === undefined || amount === undefined) {
-                throw Error('invalid address or amount');
-            }
-            const resp = sendTransaction(address, amount);
-            res.send(resp);
-        } catch (e) {
-            console.log(e.message);
-            res.status(400).send(e.message);
-        }
-    });
+      if (address === undefined || amount === undefined) {
+        throw Error('invalid address or amount');
+      }
+      const resp = sendTransaction(address, amount);
+      res.send(resp);
+    } catch (e) {
+      console.log(e.message);
+      res.status(400).send(e.message);
+    }
+  });
 
-    app.get('/transactionPool', (req, res) => {
-        res.send(getTransactionPool());
-    });
+  app.get('/transactionPool', (_: Request, res: Response) => {
+    res.send(getTransactionPool());
+  });
 
-    app.get('/peers', (req, res) => {
-        res.send(getSockets().map((s: any) => s._socket.remoteAddress + ':' + s._socket.remotePort));
-    });
-    app.post('/addPeer', (req, res) => {
-        connectToPeers(req.body.peer);
-        res.send();
-    });
+  app.get('/peers', (_: Request, res: Response) => {
+    res.send(
+      getSockets().map(
+        (s: any) => s._socket.remoteAddress + ':' + s._socket.remotePort,
+      ),
+    );
+  });
+  app.post('/addPeer', (req: Request, res: Response) => {
+    connectToPeers(req.body.peer);
+    res.send();
+  });
 
-    app.post('/stop', (req, res) => {
-        res.send({'msg' : 'stopping server'});
-        process.exit();
-    });
+  app.post('/stop', (_: Request, res: Response) => {
+    res.send({ msg: 'stopping server' });
+    process.exit();
+  });
 
-    app.listen(myHttpPort, () => {
-        console.log('Listening http on port: ' + myHttpPort);
-    });
+  app.listen(myHttpPort, () => {
+    console.log('Listening http on port: ' + myHttpPort);
+  });
 };
 
 initHttpServer(httpPort);
--- src/transactionPool.ts
+++ src/transactionPool.ts
@@ -1,71 +1,87 @@
-import * as _ from 'lodash';
-import {Transaction, TxIn, UnspentTxOut, validateTransaction} from './transaction';
+import _ from 'lodash';
+import {
+  Transaction,
+  TxIn,
+  UnspentTxOut,
+  validateTransaction,
+} from './transaction';
 
 let transactionPool: Transaction[] = [];
 
 const getTransactionPool = () => {
-    return _.cloneDeep(transactionPool);
+  return _.cloneDeep(transactionPool);
 };
 
-const addToTransactionPool = (tx: Transaction, unspentTxOuts: UnspentTxOut[]) => {
+const addToTransactionPool = (
+  tx: Transaction,
+  unspentTxOuts: UnspentTxOut[],
+) => {
+  if (!validateTransaction(tx, unspentTxOuts)) {
+    throw Error('Trying to add invalid tx to pool');
+  }
 
-    if (!validateTransaction(tx, unspentTxOuts)) {
-        throw Error('Trying to add invalid tx to pool');
-    }
-
-    if (!isValidTxForPool(tx, transactionPool)) {
-        throw Error('Trying to add invalid tx to pool');
-    }
-    console.log('adding to txPool: %s', JSON.stringify(tx));
-    transactionPool.push(tx);
+  if (!isValidTxForPool(tx, transactionPool)) {
+    throw Error('Trying to add invalid tx to pool');
+  }
+  console.log('adding to txPool: %s', JSON.stringify(tx));
+  transactionPool.push(tx);
 };
 
 const hasTxIn = (txIn: TxIn, unspentTxOuts: UnspentTxOut[]): boolean => {
-    const foundTxIn = unspentTxOuts.find((uTxO: UnspentTxOut) => {
-        return uTxO.txOutId === txIn.txOutId && uTxO.txOutIndex === txIn.txOutIndex;
-    });
-    return foundTxIn !== undefined;
+  const foundTxIn = unspentTxOuts.find((uTxO: UnspentTxOut) => {
+    return uTxO.txOutId === txIn.txOutId && uTxO.txOutIndex === txIn.txOutIndex;
+  });
+  return foundTxIn !== undefined;
 };
 
 const updateTransactionPool = (unspentTxOuts: UnspentTxOut[]) => {
-    const invalidTxs = [];
-    for (const tx of transactionPool) {
-        for (const txIn of tx.txIns) {
-            if (!hasTxIn(txIn, unspentTxOuts)) {
-                invalidTxs.push(tx);
-                break;
-            }
-        }
+  const invalidTxs = [];
+  for (const tx of transactionPool) {
+    for (const txIn of tx.txIns) {
+      if (!hasTxIn(txIn, unspentTxOuts)) {
+        invalidTxs.push(tx);
+        break;
+      }
     }
-    if (invalidTxs.length > 0) {
-        console.log('removing the following transactions from txPool: %s', JSON.stringify(invalidTxs));
-        transactionPool = _.without(transactionPool, ...invalidTxs);
-    }
+  }
+  if (invalidTxs.length > 0) {
+    console.log(
+      'removing the following transactions from txPool: %s',
+      JSON.stringify(invalidTxs),
+    );
+    transactionPool = _.without(transactionPool, ...invalidTxs);
+  }
 };
 
 const getTxPoolIns = (aTransactionPool: Transaction[]): TxIn[] => {
-    return _(aTransactionPool)
-        .map((tx) => tx.txIns)
-        .flatten()
-        .value();
+  return _(aTransactionPool)
+    .map((tx: Transaction) => tx.txIns)
+    .flatten()
+    .value();
 };
 
-const isValidTxForPool = (tx: Transaction, aTtransactionPool: Transaction[]): boolean => {
-    const txPoolIns: TxIn[] = getTxPoolIns(aTtransactionPool);
+const isValidTxForPool = (
+  tx: Transaction,
+  aTtransactionPool: Transaction[],
+): boolean => {
+  const txPoolIns: TxIn[] = getTxPoolIns(aTtransactionPool);
 
-    const containsTxIn = (txIns: TxIn[], txIn: TxIn) => {
-        return _.find(txPoolIns, ((txPoolIn) => {
-            return txIn.txOutIndex === txPoolIn.txOutIndex && txIn.txOutId === txPoolIn.txOutId;
-        }));
-    };
+  const containsTxIn = (__: TxIn[], txIn: TxIn) => {
+    return _.find(txPoolIns, (txPoolIn: TxIn) => {
+      return (
+        txIn.txOutIndex === txPoolIn.txOutIndex &&
+        txIn.txOutId === txPoolIn.txOutId
+      );
+    });
+  };
 
-    for (const txIn of tx.txIns) {
-        if (containsTxIn(txPoolIns, txIn)) {
-            console.log('txIn already found in the txPool');
-            return false;
-        }
+  for (const txIn of tx.txIns) {
+    if (containsTxIn(txPoolIns, txIn)) {
+      console.log('txIn already found in the txPool');
+      return false;
     }
-    return true;
+  }
+  return true;
 };
 
-export {addToTransactionPool, getTransactionPool, updateTransactionPool};
+export { addToTransactionPool, getTransactionPool, updateTransactionPool };
